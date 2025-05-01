42d11621284b85b278d77994b723fdcc91a000ea
Ivan Vucetic <90158455+fivan14@users.noreply.github.com>
2025-05-01 23:49:20 +0200

    add scheduled, in progress and finished descriptions and columns

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
+//dohvacanje elemenata is DOM strukture
+
 let todo = document.querySelector('.todo')
 let bar = document.querySelector('.details')
+
+let clear = document.querySelector('.delete')
 
 todo.addEventListener('click', () => {
     bar.classList.toggle('hidden')
 })
+
+//add / remove clear button on todo's
+
+todo.addEventListener('mouseenter', () => {
+    details.classList.toggle('hidden')
+    // kad miš uđe
+  });
+  
+  todo.addEventListener('mouseleave', () => {
+    details.classList.toggle('hidden')
+    // kad miš izađe
+  });
