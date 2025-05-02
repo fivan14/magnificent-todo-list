b2906966be856520008a8e8dadbf04f2452888c6
Ivan Vucetic <90158455+fivan14@users.noreply.github.com>
2025-05-02 22:46:28 +0200

    created add todo functionality/test

--- script.js
+++ script.js
@@ -1,23 +1,67 @@
 
 //dohvacanje elemenata is DOM strukture
 
+let addTodoTaskInput = document.querySelector('#add-todo-task-input')
+let addTodoButton = document.querySelector('#add-todo-button')
+let selectCategoryMain = document.querySelector('#select-category-main > select')
+
+
 let todo = document.querySelector('.todo')
 let bar = document.querySelector('.details')
-
 let clear = document.querySelector('.delete')
 
-todo.addEventListener('click', () => {
+let addSmallStep = document.querySelector('#add-small-step')
+let checkboxContainer = document.querySelector('.checkbox-container')
+let smallStepInput = document.querySelector('#small-step-input')
+
+
+// main add todo functionality
+
+addTodoButton.addEventListener('click', () => {
+    if (addTodoTaskInput.value.trim().length > 0) {
+      document.querySelector('.todo-wrapper').innerHTML += `
+              <div class="box todo">
+                ${addTodoTaskInput.value.trim()}
+                <button class="delete hidden is-pulled-right" aria-label="close"></button>
+             </div>`
+
+      addTodoTaskInput.value = ''
+    }
+})
+
+// event click i dodavanje small stepa
+
+addSmallStep.addEventListener('click', () => {
+    if (smallStepInput.value.trim().length > 0) {
+        checkboxContainer.innerHTML = (`
+                  <label class="checkbox">
+                      <input type="checkbox" />
+                        ${smallStepInput.value}
+                  </label>` + checkboxContainer.innerHTML)
+        smallStepInput.value = ''
+    }
+})
+
+//---------------------------------------
+
+todo.addEventListener('click', (ele) => {
+    if (ele.target.matches('.delete')) {
+        document.querySelector('.todo-wrapper').remove()
+    }
     bar.classList.toggle('hidden')
 })
 
 //add / remove clear button on todo's
 
 todo.addEventListener('mouseenter', () => {
-    details.classList.toggle('hidden')
+    clear.classList.toggle('hidden')
     // kad miš uđe
   });
   
   todo.addEventListener('mouseleave', () => {
-    details.classList.toggle('hidden')
+    clear.classList.toggle('hidden')
     // kad miš izađe
   });
+
+
+  //delete todo
