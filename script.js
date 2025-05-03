d849d24ae881a9688da66d8a744a1c8ca5dc4678
Ivan Vucetic <90158455+fivan14@users.noreply.github.com>
2025-05-03 02:03:58 +0200

    add event delegation on todo-wrapper, and fixed clicks on elements

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ let addTodoButton = document.querySelector('#add-todo-button')
 let selectCategoryMain = document.querySelector('#select-category-main > select')
 
 
+let todoWrapper = document.querySelector('.todo-wrapper')
 let todo = document.querySelector('.todo')
-let bar = document.querySelector('.details')
+let detailsContainer = document.querySelector('.details')
 let clear = document.querySelector('.delete')
 
 let addSmallStep = document.querySelector('#add-small-step')
@@ -20,10 +21,33 @@ let smallStepInput = document.querySelector('#small-step-input')
 addTodoButton.addEventListener('click', () => {
     if (addTodoTaskInput.value.trim().length > 0) {
       document.querySelector('.todo-wrapper').innerHTML += `
-              <div class="box todo">
-                ${addTodoTaskInput.value.trim()}
-                <button class="delete hidden is-pulled-right" aria-label="close"></button>
-             </div>`
+              <div class="todo-item">
+                <div class="box todo">
+                  ${addTodoTaskInput.value.trim()}
+                  <button class="delete hidden is-pulled-right" aria-label="close"></button>
+                </div>
+  
+  <!-- container / progress bar sa detaljima i koracima za HIDE/SHOW -->
+                <div class="container details fade-slide hidden">
+                  
+                <div class="container mb-4 is-flex is-justify-content-space-between">
+                    
+                  <input id="small-step-input" class="input is-small" type="text" placeholder="Add small step"/>
+                  <button id="add-small-step" class="button is-small is-danger">Add Step</button>  
+                  
+                </div>  
+                <div class="checkbox-container container is-flex is-flex-direction-column">
+                    <label class="checkbox">
+                      <input type="checkbox" checked />
+                      Instalirati Bulmu i upoznati klase
+                    </label>
+                    <label class="checkbox">
+                      <input type="checkbox" />
+                      Prouciti flexbox
+                    </label>
+                </div>
+                <progress class="progress is-danger bar mt-2 mb-4" value="50" max="100"></progress>
+              </div>`
 
       addTodoTaskInput.value = ''
     }
@@ -31,24 +55,47 @@ addTodoButton.addEventListener('click', () => {
 
 // event click i dodavanje small stepa
 
-addSmallStep.addEventListener('click', () => {
-    if (smallStepInput.value.trim().length > 0) {
-        checkboxContainer.innerHTML = (`
-                  <label class="checkbox">
-                      <input type="checkbox" />
-                        ${smallStepInput.value}
-                  </label>` + checkboxContainer.innerHTML)
-        smallStepInput.value = ''
-    }
-})
+// addSmallStep.addEventListener('click', () => {
+//     if (smallStepInput.value.trim().length > 0) {
+//         checkboxContainer.innerHTML = (`
+//                   <label class="checkbox">
+//                       <input type="checkbox" />
+//                         ${smallStepInput.value}
+//                   </label>` + checkboxContainer.innerHTML)
+//         smallStepInput.value = ''
+//     }
+// })
 
 //---------------------------------------
 
-todo.addEventListener('click', (ele) => {
-    if (ele.target.matches('.delete')) {
-        document.querySelector('.todo-wrapper').remove()
+// add event to show details on click
+
+todoWrapper.addEventListener('click', (e) => {
+    const todoItem = e.target.closest('.todo-item')
+    if (!todoItem) return 
+
+    if (e.target.closest('.todo')) {
+      const details = todoItem.querySelector('.details')
+      details.classList.toggle('hidden')
     }
-    bar.classList.toggle('hidden')
+
+    if (e.target.closest('#add-small-step')) {
+      console.log('Small button clicked!')
+      const checkboxContainer = todoItem.querySelector('.checkbox-container')
+      const input = todoItem.querySelector('#small-step-input')
+
+      if (input.value) {
+        checkboxContainer.innerHTML += `
+                    <label class="checkbox">
+                      <input type="checkbox" />
+                      ${input.value.trim()}
+                    </label> `
+
+        checkboxContainer.innerHTML = ''
+      }
+      
+    }
+    
 })
 
 //add / remove clear button on todo's
